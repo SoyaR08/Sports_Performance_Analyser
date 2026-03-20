9f70bd56a21cb6e323407ddec3134b874d64caed
SoyaR08 <rafa140200@gmail.com>
2026-03-20 02:24:35 +0100

    I think i did it

---
+++ clean_events.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import json
+import os
+
+
+def clean_events(path):
+
+    final_df = None
+    summaries = []
+
+    for file in os.listdir(path):
+
+        if file.endswith(".json"):
+
+            filepath = os.path.join(path, file)
+
+            with open(filepath, "r", encoding="utf-8") as f:
+                data = json.load(f)
+
+            df = pd.json_normalize(data)
+
+            # Asegurar columnas
+            for col in [
+                "bad_behaviour.card.name",
+                "foul_committed.card.name",
+                "shot.outcome.name"
+            ]:
+                if col not in df.columns:
+                    df[col] = None
+
+            shots = df[df["type.name"] == "Shot"]
+            shots_on_target = shots[
+                shots["shot.outcome.name"].isin(["Goal", "Saved", "Saved to Post"])
+            ]
+
+
+            yellow_cards_fouls = df[
+                (df["type.name"] == "Foul Committed") &
+                (df["foul_committed.card.name"] == "Yellow Card")
+            ]
+
+            # Ahora ya puedes filtrar sin miedo
+            yellow_cards_behaviour = df[
+                (df["type.name"] == "Bad Behaviour") &
+                (df["bad_behaviour.card.name"] == "Yellow Card")
+            ]
+
+            red_cards = df[
+                df["bad_behaviour.card.name"].isin(["Red Card", "Second Yellow"])
+            ]
+
+            shots_by_team = shots.groupby("team.name").size()
+            shots_on_target_by_team = shots_on_target.groupby("team.name").size()
+            yellow_by_team = pd.concat([yellow_cards_fouls, yellow_cards_behaviour]).groupby("team.name").size()
+            red_by_team = red_cards.groupby("team.name").size()
+
+            match_id = file.replace(".json", "")
+
+            summary = pd.DataFrame({
+                "shots": shots_by_team,
+                "shots_on_target": shots_on_target_by_team,
+                "yellow_cards": yellow_by_team,
+                "red_cards": red_by_team
+            }).fillna(0)
+
+            summary["match_id"] = match_id
+            summary = summary.reset_index().rename(columns={"index": "team"})
+
+            summaries.append(summary)
+
+    final_df = pd.concat(summaries, ignore_index=True, sort=False)
+    return final_df
+
+os.makedirs("datasets/events", exist_ok=True)
+
+events_df = clean_events("cleaned_data/cleaned_events")
+
+events_df.to_csv("datasets/events/events.csv", index=False)
