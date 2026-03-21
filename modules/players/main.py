894656e00a5e152d84a148f0a076d71797159473
SoyaR08 <rafa140200@gmail.com>
2026-03-21 19:09:29 +0100

    Encapsuled this for working as a module

--- modules/players/main.py
+++ modules/players/main.py
@@ -1,14 +1,18 @@
-from libraries import *
+from .libraries import *
 import os
 
-os.makedirs("final_data/tests/players", exist_ok=True)
-os.makedirs("final_data/tests/players", exist_ok=True)
+def build_environment():
 
-player_events_df = getPlayerEvents("cleaned_data/cleaned_events")
+    os.makedirs("final_data/tests4/players", exist_ok=True)
 
-player_events_df.to_csv("final_data/tests/players/players_events_try.csv", index=False)
+    player_events_df = getPlayerEvents("cleaned_data/cleaned_events")
 
+    player_events_df.to_csv("final_data/tests4/players/players_events_try.csv", index=False)
 
-player_minutes_df = getLineups("cleaned_data/cleaned_lineups")
+    print("Archivo players_events_try.csv creado")
 
-player_minutes_df.to_csv("final_data/tests/players/players_minutes.csv", index=False)
+    player_minutes_df = getLineups("cleaned_data/cleaned_lineups")
+
+    player_minutes_df.to_csv("final_data/tests4/players/players_minutes.csv", index=False)
+
+    print("Archivo players_minutes.csv creado")
