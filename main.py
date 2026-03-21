66771f59a5bc698c8f06a0bbcd2e04159fa4238a
SoyaR08 <rafa140200@gmail.com>
2026-03-21 20:19:00 +0100

    more modules

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+from load import load_and_create_main_files
 from modules.players import build_environment
-
+from modules.events import build_events_environment
 
 if __name__ == "__main__":
+
+    # Solo descomentar cuando se tengan todos los archivos del dataset StatsBomb Dataset
+    # load_and_create_main_files()
+
+    build_events_environment()
     build_environment()
