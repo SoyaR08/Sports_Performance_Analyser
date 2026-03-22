4c0db82818d0d33225b324a1255548f5c80177e1
SoyaR08 <rafa140200@gmail.com>
2026-03-22 17:04:00 +0100

    Updated

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from load import load_and_create_main_files
 from modules.players import build_environment
 from modules.events import build_events_environment
+from modules.matches import build_matches_environment
 
 if __name__ == "__main__":
 
     # Solo descomentar cuando se tengan todos los archivos del dataset StatsBomb Dataset
     # load_and_create_main_files()
 
+    build_matches_environment()
     build_events_environment()
     build_environment()
